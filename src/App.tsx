434b2032c8837072994dcc674d849b232d7008e2
EnzoFranchin <enzofranchindesouza336@gmail.com>
2025-11-23 20:25:28 -0300

    Css e Rotas das paginas ajustadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './routes/Home'
+import Dashboard from './routes/Dashboard'
+import Authpage from './routes/Authpage'   
+import Humor from './routes/Humor'
+import Conteudos from './routes/Conteudos'
+import Lembretes from './routes/Lembretes'
+import Sobre from './routes/Sobre'
+import FAQ from './routes/FAQ'
+import Contato from './routes/Contato'
+import Integrantes from './routes/Integrantes'
+import Error from './routes/Error'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="flex flex-col min-h-screen bg-white">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/authpage" element={<Authpage />} />   
+          <Route path="/humor" element={<Humor />} />
+          <Route path="/conteudos" element={<Conteudos />} />
+          <Route path="/lembretes" element={<Lembretes />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/integrantes" element={<Integrantes />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
